aa1830802b161aada61384590e16a5caf08dd426
Shaddy <shadybett540@gmail.com>
2023-10-24 22:38:20 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
+    <h1>Hello world</h1>
+    
   </React.StrictMode>
 );
 
