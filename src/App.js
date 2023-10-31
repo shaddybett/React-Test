7e48cced5d8e74f7eac020dc9147e04edec5d230
Shaddy <shadybett540@gmail.com>
2023-10-31 12:15:06 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState } from 'react'
 
-function App() {
+export default function App() {
+  const [initial,setInitial] = useState(0)
+  function handleSubmit(){
+  setInitial(initial+1)
+  }
+  function handleSubmitB(){
+    setInitial(initial-1)
+    console.log('clicked1')
+   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleSubmit}>+</button>
+      <span>{initial}</span>
+      <button onClick={handleSubmitB}>-</button>
     </div>
-  );
+  )
 }
 
-export default App;
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     
-    <h1>Hello world</h1>
+   <App />
     
   </React.StrictMode>
 );
